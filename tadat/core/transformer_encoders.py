8c85870bf193206f183a137ff90e3cda2dc7f101
Silvio Moreira <silvio.moreira@qntfy.com>
2020-11-05 16:25:08 -0800

    added automodels

--- tadat/core/transformer_encoders.py
+++ tadat/core/transformer_encoders.py
@@ -2,7 +2,7 @@ import numpy as np
 import time
 import torch
 import torch.nn as nn
-from transformers import BertTokenizer, BertModel
+from transformers import AutoTokenizer, AutoModel
 import sys
 import random 
 import os
@@ -24,15 +24,22 @@ def __get_device(silent=True):
             print('No GPU available, using the CPU instead.')        
     return device
 
-def get_BERT(pretrained_model=DEFAULT_BERT_MODEL, hidden_states=False):
+def get_encoder(pretrained_model=DEFAULT_BERT_MODEL, hidden_states=False):
     # Load pre-trained model tokenizer (vocabulary)
-    tokenizer = BertTokenizer.from_pretrained(pretrained_model)
+    tokenizer = AutoTokenizer.from_pretrained(pretrained_model)
     # Load pre-trained model (weights)
-    model = BertModel.from_pretrained(pretrained_model, output_hidden_states=hidden_states)
+    model = AutoModel.from_pretrained(pretrained_model, output_hidden_states=hidden_states)
     return tokenizer, model
 
-def encode_sequences(docs, tokenizer=None, encoder=None, cls_features=False, 
-                    batchsize=DEFAULT_BATCH_SIZE, device=None):  
+def encode_sequences(docs, tokenizer=None, encoder=None, max_input_size=BERT_MAX_INPUT,
+                    cls_features=False, batchsize=DEFAULT_BATCH_SIZE, device=None):  
+    #BERT
+    if not tokenizer:
+        tokenizer = AutoTokenizer.from_pretrained(DEFAULT_BERT_MODEL)
+    if not encoder:
+        encoder = AutoModel.from_pretrained(DEFAULT_BERT_MODEL, output_hidden_states=False)
+    if not device:
+        device = __get_device()
     feature_vectors = []    
     n_batches = int(len(docs)/batchsize)+1
     for j in range(n_batches):
@@ -40,22 +47,14 @@ def encode_sequences(docs, tokenizer=None, encoder=None, cls_features=False,
         if len(batch) > 0:
             sys.stdout.write("\nbatch:{}/{} (size: {})".format(j+1,n_batches, str(len(batch))))
             sys.stdout.flush()
-            cls_vec, pool_vec = sequence_features(batch,tokenizer, encoder,device)            
+            cls_vec, pool_vec = sequence_features(batch,tokenizer, encoder, max_input_size, device)            
             feats = cls_vec if cls_features else pool_vec                       
             feature_vectors.append(feats)                
     feature_vectors = np.vstack(feature_vectors)
 
     return feature_vectors
 
-def sequence_features(docs, tokenizer=None, encoder=None, device=None):    
-    #BERT
-    if not tokenizer:
-        tokenizer = BertTokenizer.from_pretrained(DEFAULT_BERT_MODEL)
-    if not encoder:
-        encoder = BertModel.from_pretrained(DEFAULT_BERT_MODEL, output_hidden_states=False)
-    if not device:
-        device = __get_device()
-
+def sequence_features(docs, tokenizer, encoder, max_input_size, device):    
     docs_tensor = []    
     tokenized_docs = []    
     bertify = "[CLS] {} [SEP]"  
@@ -63,7 +62,7 @@ def sequence_features(docs, tokenizer=None, encoder=None, device=None):
     #count the document lengths  
     max_len = max([len(d) for d in tokenized_docs]) 
     #document cannot exceed BERT input matrix size 
-    max_len = min(BERT_MAX_INPUT, max_len)
+    max_len = min(max_input_size, max_len)
     print("[max len: {}]".format(max_len))
     for doc in tokenized_docs:   
         # Convert tokens to vocabulary indices
@@ -91,8 +90,8 @@ def sequence_features(docs, tokenizer=None, encoder=None, device=None):
     return cls_features.cpu().numpy(), pool_features.cpu().numpy()
 
 def encode_multi_sequences(docs, max_sequences, tokenizer=None, encoder=None,
-                            cls_features=False, batchsize=DEFAULT_BATCH_SIZE, 
-                            tmp_path=None, device=None):  
+                            max_input_size=BERT_MAX_INPUT, cls_features=False, 
+                            batchsize=DEFAULT_BATCH_SIZE, tmp_path=None, device=None):  
     tmp_fname = None      
     if tmp_path:        
         dirname = os.path.dirname(tmp_path)
@@ -101,20 +100,23 @@ def encode_multi_sequences(docs, max_sequences, tokenizer=None, encoder=None,
         tmp_fname = tmp_path+"/"+str(uuid.uuid4())
         print("[saving @ {}]".format(tmp_fname))       
     if not tokenizer:
-        tokenizer = BertTokenizer.from_pretrained(DEFAULT_BERT_MODEL)
+        tokenizer = AutoTokenizer.from_pretrained(DEFAULT_BERT_MODEL)
     if not encoder:
-        encoder = BertModel.from_pretrained(DEFAULT_BERT_MODEL, output_hidden_states=False)
+        encoder = AutoModel.from_pretrained(DEFAULT_BERT_MODEL, output_hidden_states=False)
     if not device:
         device = __get_device()
-    vectors = []      
+    vectors = []          
     n_batches = int(len(docs)/batchsize)+1
     for j in range(n_batches):
         batch = docs[batchsize*j:batchsize*(j+1)]
         if len(batch) > 0:
             sys.stdout.write("\nbatch:{}/{} (size: {})".format(j+1,n_batches, str(len(batch))))
             sys.stdout.flush()
-            cls_vec, pool_vec = multi_sequence_features(batch, max_sequences,tokenizer, encoder,device)                        
-            feats = cls_vec if cls_features else pool_vec                       
+            cls_vec, pool_vec = multi_sequence_features(batch, max_sequences, 
+                                                        tokenizer, encoder, max_input_size,
+                                                        device)                        
+            feats = cls_vec if cls_features else pool_vec                                   
+            
             if tmp_fname:                    
                 #save partial features     
                 np.save(tmp_fname+"_"+str(j), feats)
@@ -128,31 +130,31 @@ def encode_multi_sequences(docs, max_sequences, tokenizer=None, encoder=None,
                 x = np.load(f)
                 vectors.append(x)                
             #remove temporary file
-            # os.remove(tmp_fname+"_{}.npy".format(j))
+            os.remove(tmp_fname+"_{}.npy".format(j))
         
-    vectors = np.vstack(vectors)        
+    vectors = np.vstack(vectors)            
     return vectors
 
-def multi_sequence_features(docs, max_sequences, tokenizer, encoder, device):    
+def multi_sequence_features(docs, max_sequences, tokenizer, encoder, max_input_size, device):    
     docs_tensor = []    
     #keep track of the number of sequences of each document
     doc_n_sequences = []        
     #tokenize documents
     tokenized_docs = [tokenizer.tokenize(doc) for doc in docs]     
     for doc in tokenized_docs:           
-        sequence_tensor = torch.zeros([max_sequences, BERT_MAX_INPUT], dtype=torch.long)        
+        sequence_tensor = torch.zeros([max_sequences, max_input_size], dtype=torch.long)        
         # split doc into sequences of size BERT_MAX_INPUT
-        n_seqs = int(len(doc)/BERT_MAX_INPUT)+1        
+        n_seqs = int(len(doc)/max_input_size)+1        
         n_seqs = min(n_seqs, max_sequences)
         doc_n_sequences.append(n_seqs)        
         for i in range(n_seqs):
             #using (BERT_MAX_INPUT-2) to leave space for [CLS]/[SEP] tokens
-            seq = doc[i*(BERT_MAX_INPUT-2):(i+1)*(BERT_MAX_INPUT-2)]
+            seq = doc[i*(max_input_size-2):(i+1)*(max_input_size-2)]
             seq = ["[CLS]"] + seq + ["[SEP]"]            
             # Convert tokens to vocabulary indices
             idxs = tokenizer.convert_tokens_to_ids(seq)        
             #add padding to indexed tokens
-            pad_size = BERT_MAX_INPUT - len(idxs)            
+            pad_size = max_input_size - len(idxs)            
             idxs+=[0] * pad_size            
             sequence_tensor[i, :] = torch.tensor([idxs])            
         docs_tensor.append(sequence_tensor)         
@@ -179,10 +181,9 @@ def multi_sequence_features(docs, max_sequences, tokenizer, encoder, device):
             n_seqs = torch.tensor(doc_n_sequences[j], dtype=torch.long, device=device)            
             #zero out (pad) unused sequences  
             pool_x[n_seqs:, :] = 0
-            cls_x[n_seqs:, :] = 0
+            cls_x[n_seqs:, :] = 0            
             pool_features.append(pool_x)
             cls_features.append(cls_x)
     pool_features = torch.stack(pool_features).cpu().numpy()
-    cls_features = torch.stack(cls_features).cpu().numpy()
+    cls_features = torch.stack(cls_features).cpu().numpy()        
     return pool_features, cls_features
-    # return cls_features.cpu().numpy(), pool_features.cpu().numpy()
